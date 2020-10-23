38618c630a1f14d5d2b1cb1ce2393639dfba8f09
Boris Gvozdev <bgvozdev@atlassian.com>
2020-10-24 08:22:37 +1100

    joplin: add pages

--- editor/src/index.js
+++ editor/src/index.js
@@ -5,8 +5,31 @@ import EditorLoader from './EditorLoader';
 
 InitEditor('editorPlaceholder',
     {
-        "fragments": [],
-        "pages": [],
+        "fragments": [
+            {
+                "pages": [],
+                "start": "00:0.0",
+                "end": "00:10.0"
+            }
+        ],
+        "pages": [
+            {
+                "url": "https://images2.imgbox.com/53/8c/z2gPCYKC_o.jpg?download=true",
+                "id": "1603488067720"
+            },
+            {
+                "url": "https://images2.imgbox.com/c0/db/OWDIpnQe_o.jpg?download=true",
+                "id": "1603488076637"
+            },
+            {
+                "url": "https://images2.imgbox.com/2b/8f/9IKD1rL7_o.jpg?download=true",
+                "id": "1603488084988"
+            },
+            {
+                "url": "https://images2.imgbox.com/3b/4a/Wqsk7RkM_o.jpg?download=true",
+                "id": "1603488094313"
+            }
+        ],
         "videoUrl": "https://www.youtube.com/watch?v=RMnlu50CBb8"
     }
 );
