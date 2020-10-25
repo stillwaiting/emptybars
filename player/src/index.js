6f117b53cf369b41fc314fd7ea0be9fa1b08544e
Boris Gvozdev <bgvozdev@atlassian.com>
2020-10-25 11:44:37 +1100

    directory rename

--- player/src/index.js
+++ player/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Editor from './editor/Editor';
+import Editor from './player/Editor';
 import {secsToString, stringToSecs, transformFromHumanReadable} from "emptybars-common/utils";
 
 InitPlayer('playerPlaceholder',
