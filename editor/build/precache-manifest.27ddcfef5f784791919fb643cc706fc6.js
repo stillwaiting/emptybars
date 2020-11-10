ba5ac82d27befbd53654c736d86deb949335cccb
Boris Gvozdev <bgvozdev@atlassian.com>
2020-11-10 19:57:25 +1100

    build editor

---
+++ editor/build/precache-manifest.27ddcfef5f784791919fb643cc706fc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5f36ae2476558abac7a0699df983fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68169211ed839d6dd461",
+    "url": "/static/css/main.319f7ece.chunk.css"
+  },
+  {
+    "revision": "7c96d92eb3a2504196aa",
+    "url": "/static/js/2.5e4fc012.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5e4fc012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68169211ed839d6dd461",
+    "url": "/static/js/main.5d7a75fe.chunk.js"
+  },
+  {
+    "revision": "b5059f6d1e47dae3ed63",
+    "url": "/static/js/runtime-main.cff50e6e.js"
+  }
+]);
