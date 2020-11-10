c1f8da8885e9eddeeda1c90259cedb02e3dde348
Boris Gvozdev <bgvozdev@atlassian.com>
2020-11-10 20:00:07 +1100

    build editor

--- editor/build/service-worker.js
+++ editor/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27ddcfef5f784791919fb643cc706fc6.js"
+  "./precache-manifest.199dd68975abc2f234aa01ca2750ca9f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
