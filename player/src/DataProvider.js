c6d4d1d852d8fc6e6f52a5a877e8ac6efb3ff574
Boris Gvozdev <bgvozdev@atlassian.com>
2020-12-15 17:50:40 +1100

    update text

--- player/src/DataProvider.js
+++ player/src/DataProvider.js
@@ -5,8 +5,8 @@ import './DataProvider.scss';
 export default function({onDataProvided}) {
     const ref = useRef(null);
     return <div class='dataProvider'>
-        <h2>Paste segments data to play:</h2>
-        <div><textarea ref={ref} placeholder='Paste segments data to play here'></textarea></div>
+        <h2>Paste video sections data to play:</h2>
+        <div><textarea ref={ref} placeholder='Paste video sections data to play here'></textarea></div>
         <div><button onClick={() => {
             onDataProvided(JSON.parse(ref.current.value));
         }}>Play</button></div>
