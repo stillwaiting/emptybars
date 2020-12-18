95e4dcace81a6eb976e41872519f1fb6b607b9fa
Natallia <natallia@Natallias-MBP.modem>
2020-12-18 20:26:58 +1100

    make header responsive

--- gatsby/src/components/header.js
+++ gatsby/src/components/header.js
@@ -1,52 +1,34 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import Image from "./image";
+import Image from "./image"
 
 const Header = ({ composition, composer, performer }) => (
-  <header
-    style={{
-      background: "darkblue",
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        display: "flex",
-        maxWidth: 960,
-        padding: `0.5rem 0 0.5rem 0`,
-      }}
-    >
-
-        <div>
-            <Link
-              to="/"
-              style={{
-                color: `white`,
-                textDecoration: `none`,
-              }}
-            >
-            <Image />
-            </Link>
-        </div>
-        <div style={{
-
-        }}>
-            {composition
-                ?  <div style={{
-                    color: "white",
-                    paddingLeft: "20px"
-                }}>
-                        <h1 style={{
-                            margin:"0px",
-                            paddingTop: "20px",
-                        }}>{composition} {composer ? <span>({composer})</span> : ''}</h1>
-                    {performer ? <span>Performed by {performer}</span> : ''}
-                    </div>
-                : ''
-            }
-        </div>
+  <header>
+    <div>
+      <div>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          <Image />
+        </Link>
+      </div>
+      <div>
+        {composition ? (
+          <div>
+            <h1>
+              {composition} {composer ? <span>({composer})</span> : ""}
+            </h1>
+            {performer ? <span>Performed by {performer}</span> : ""}
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   </header>
 )
